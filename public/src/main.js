91f4c13e1a5b8f6fd2c27cd2c03a8812bcc8330a
Leon Kipkoech <leonkipkip@gmail.com>
2021-09-14 11:31:26 -0400

    fixed links

--- public/src/main.js
+++ public/src/main.js
@@ -1,14 +1,18 @@
-function scroll(){
-    var pageHeight= window.innerWidth;
-    window.scrollTo(0, 1000);
+function start(){
+    getTime();
+   checkFonts();
+
 }
-function getTime(){
-    
-    
-    var today = new Date();
-    
+function getTime(){ 
+    var today = new Date(); 
     var hrs  = today.getHours();
-var time = hrs.toString() + today.getMinutes().toString() + " HRS";
-var timeElement = document.getElementById("time");
-timeElement.innerHTML = time;
+    var time = hrs.toString() + today.getMinutes().toString() + " HRS";
+    var timeElement = document.getElementById("time");
+    timeElement.innerHTML = time;
+}
+function checkFonts(){
+    document.fonts.ready.then(function () {
+        var loader = document.getElementById("load");
+        loader.style.display = "none";
+     });
 }
